c658f064375d8bab80ddf5a37b030c0a2c46a6b8
tbceia <122027051+tbceia@users.noreply.github.com>
2023-01-05 15:13:40 +0000

    Update seonplugin.js

--- www/seonplugin.js
+++ www/seonplugin.js
@@ -1,1 +1,21 @@
+// Empty constructor
+function SeonPlugin() {}
 
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+SeonPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'SeonPlugin', 'show', [options]);
+}
+
+// Installation constructor that binds ToastyPlugin to window
+SeonPlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.seonPlugin = new SeonPlugin();
+  return window.plugins.seonPlugin;
+};
+cordova.addConstructor(SeonPlugin.install);
