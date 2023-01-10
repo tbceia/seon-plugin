e2ca95136c2d230ebb99f0ba883d920ca3ed7c1e
tbceia <122027051+tbceia@users.noreply.github.com>
2023-01-10 12:51:17 +0000

    Update seonplugin.js

--- www/seonplugin.js
+++ www/seonplugin.js
@@ -2,12 +2,10 @@
 function SeonPlugin() {}
 
 // The function that passes work along to native shells
-// Message is a string, duration may be 'long' or 'short'
-SeonPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+SeonPlugin.prototype.getDeviceFingerprint = function(sessionId, successCallback, errorCallback) {
   var options = {};
-  options.message = message;
-  options.duration = duration;
-  cordova.exec(successCallback, errorCallback, 'SeonPlugin', 'show', [options]);
+  options.sessionId = sessionId;
+  cordova.exec(successCallback, errorCallback, 'SeonPlugin', 'getDeviceFingerprint', [options]);
 }
 
 // Installation constructor that binds SeonPlugin to window
